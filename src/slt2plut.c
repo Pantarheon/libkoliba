90bd6626c371f73e019bc3d197371528ae150897
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-11-21 15:05:20 -0600

    Changed slt2plut.c for new capitalization.

--- src/slt2plut.c
+++ src/slt2plut.c
@@ -2,7 +2,7 @@
 
 	slt2plut.c
 
-	Copyright 2019 G. Adam Stanislav
+	Copyright 2019-2021 G. Adam Stanislav
 	All rights reserved
 
 	Redistribution and use in source and binary forms,
@@ -54,14 +54,14 @@ KLBDC KOLIBA_PLUT * KOLIBA_ConvertSlutToPlut(KOLIBA_PLUT *plut, const KOLIBA_SLU
 
 	if ((plut == NULL) || (slut == NULL)) return NULL;
 
-	memcpy(&p.black, &slut->black, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.white, &slut->white, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.red, &slut->red, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.green, &slut->green, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.blue, &slut->blue, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.cyan, &slut->cyan, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.magenta, &slut->magenta, sizeof(KOLIBA_VERTEX));
-	memcpy(&p.yellow, &slut->yellow, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Black,   &slut->Black, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.White,   &slut->White, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Red,     &slut->Red, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Green,   &slut->Green, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Blue,    &slut->Blue, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Cyan,    &slut->Cyan, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Magenta, &slut->Magenta, sizeof(KOLIBA_VERTEX));
+	memcpy(&p.Yellow,  &slut->Yellow, sizeof(KOLIBA_VERTEX));
 	p.divisor = 1.0;
 	p.efficacy = 1.0;
 	return memcpy(plut, &p, sizeof(KOLIBA_PLUT));
