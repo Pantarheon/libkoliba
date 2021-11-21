215931198c91442af042521784ac17a794f6d3ee
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-11-21 12:55:45 -0600

    Changed flutms.c for new capitalization.

--- src/flutms.c
+++ src/flutms.c
@@ -2,7 +2,7 @@
 
 	flutms.c
 
-	Copyright 2019 G. Adam Stanislav
+	Copyright 2019-2021 G. Adam Stanislav
 	All rights reserved
 
 	Redistribution and use in source and binary forms,
@@ -53,18 +53,18 @@ KLBDC KOLIBA_FLUT * KOLIBA_ConvertMatrixToFlut(KOLIBA_FLUT * fLut, const KOLIBA_
 	if ((fLut == NULL) || (mat == NULL)) return NULL;
 
 	memcpy	(fLut, &KOLIBA_Zeroes, sizeof(KOLIBA_FLUT));
-	fLut->black.r = mat->red.o;
-	fLut->black.g = mat->green.o;
-	fLut->black.b = mat->blue.o;
-	fLut->red.r = mat->red.r;
-	fLut->red.g = mat->green.r;
-	fLut->red.b = mat->blue.r;
-	fLut->green.r = mat->red.g;
-	fLut->green.g = mat->green.g;
-	fLut->green.b = mat->blue.g;
-	fLut->blue.r = mat->red.b;
-	fLut->blue.g = mat->green.b;
-	fLut->blue.b = mat->blue.b;
+	fLut->Black.r = mat->red.o;
+	fLut->Black.g = mat->green.o;
+	fLut->Black.b = mat->blue.o;
+	fLut->Red.r   = mat->red.r;
+	fLut->Red.g   = mat->green.r;
+	fLut->Red.b   = mat->blue.r;
+	fLut->Green.r = mat->red.g;
+	fLut->Green.g = mat->green.g;
+	fLut->Green.b = mat->blue.g;
+	fLut->Blue.r  = mat->red.b;
+	fLut->Blue.g  = mat->green.b;
+	fLut->Blue.b  = mat->blue.b;
 
 	return fLut;
 }
