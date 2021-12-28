4c49984cc4b6ea43df5cd963cff0597bd554eff9
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-12-27 20:00:09 -0600

    Added KOLIBA_AngleMultiply()

---
+++ src/anglemul.c
@@ -0,0 +1,58 @@
+/*
+
+	anglemul.c
+
+	Copyright 2021 G. Adam Stanislav
+	All rights reserved
+
+	Redistribution and use in source and binary forms,
+	with or without modification, are permitted provided
+	that the following conditions are met:
+
+	1. Redistributions of source code must retain the
+	above copyright notice, this list of conditions
+	and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the
+	above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or
+	other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the
+	names of its contributors may be used to endorse or
+	promote products derived from this software without
+	specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+	AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+	SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+	STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+	OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "koliba.h"
+#include <math.h>
+
+#if !defined(NULL)
+	#define	NULL	((void*)0)
+#endif
+
+// Multiply an angle by a constant.
+KLBDC KOLIBA_ANGLE * KOLIBA_AngleMultiply(KOLIBA_ANGLE * kAng, const KOLIBA_ANGLE * const multiplicand, double multiplier) {
+	if ((kAng == NULL) || (multiplicand == NULL)) return NULL;
+
+	kAng->angle = multiplicand->angle * multiplier;
+	if (kAng != multiplicand)
+		kAng->units = multiplicand->units;
+	return kAng;
+}
